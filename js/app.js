e97a9d4ca696f2ecbcad950356eb71a3eeec0f40
Miguel Guajardo <miguelguajardo1468@hotmail.com>
2022-06-20 15:15:01 -0300

    Terminando detalles

--- js/app.js
+++ js/app.js
@@ -1,58 +1,66 @@
-let usuario__name = localStorage.getItem("nombre__usuario");
-let nombreUsuario = document.getElementById("nombreUsuario");
-nombreUsuario.innerText="PERSONAL-NOTES DE" + usuario__name;
-let borrarNotas = document.getElementById("BorrarNotas");
-borrarNotas.addEventListener("click", ()=>{
-    localStorage.removeItem("DataDatos");
-    location.reload();
+const usuario__name = localStorage.getItem("nombre__usuario");
+const nombreUsuario = document.getElementById("nombreUsuario");
+nombreUsuario.innerText = "PERSONAL-NOTES DE " + usuario__name;
+
+window.onload = insertarImagenFondo;
+const borrarNotas = document.getElementById("BorrarNotas");
+borrarNotas.addEventListener("click", () => {
+  localStorage.removeItem("DataDatos");
+  location.reload();
 });
-let cerrar_sesion = document.getElementById("cerrar_sesion") ;
-cerrar_sesion.addEventListener("click", ()=>{
-    Swal.fire({
-        text:"Esta seguro de salir de la sesion",
-        confirmButtonText: "Cerrar sesion",
-        cancelButtonText: "Cancelar",
-        showCancelButton: true,
-    }).then((result)=>{
-        if(result.isConfirmed){
-            window.location = "index.html";
-        }else{
-            location.reload();
-        }
-    })
+const cerrar_sesion = document.getElementById("cerrar_sesion");
+cerrar_sesion.addEventListener("click", () => {
+  Swal.fire({
+    text: "Esta seguro de salir de la sesion",
+    confirmButtonText: "Cerrar sesion",
+    cancelButtonText: "Cancelar",
+    showCancelButton: true,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      window.location = "index.html";
+    } else {
+      location.reload();
+    }
+  });
 });
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    let noteList = document.getElementById("note-list")
-    let myObjArray = JSON.parse(localStorage.getItem("DataDatos"));
-    myObjArray.forEach(arrayElement => {
-        const div = document.createElement("div");
-    div.innerHTML = `
-        <div class="card text-center mb-4 pt-3">
+document.addEventListener("DOMContentLoaded", () => {
+  let noteList = document.getElementById("note-list");
+  let myObjArray = JSON.parse(localStorage.getItem("DataDatos")) || [];
+  myObjArray.forEach((arrayElement) => {
+    const div = document.createElement("div");
+    div.innerHTML =
+      `
+        <div class="card text-center pt-3">
             <div class="card-Body">
             <p class="border d-inline p-2 mt-3 bg-dark text-light">
-            <span id="valorDate">` + arrayElement.Date + `</span>
+            <span id="valorDate">` +
+      arrayElement.Date +
+      `</span>
             </p>
             <p class="mt-3 font-italic fs-2">
-            <span id="valorNote">` + arrayElement.Note + `</span>
+            <span id="valorNote">` +
+      arrayElement.Note +
+      `</span>
             </p>
+            </div>
                 </div>
-        </div>
     `;
     noteList.appendChild(div);
-    })});
+  });
+});
 
-class Nota{
-    constructor(date,text){
-        this.date = date;
-        this.text = text;
-    }
+class Nota {
+  constructor(date, text) {
+    this.date = date;
+    this.text = text;
+  }
 }
-class Intefaz{
-    agregarNota(nota){
-        const noteList =  document.getElementById("note-list");
-        const div = document.createElement("div");
-        div.innerHTML = `
+class Interfaz {
+  agregarNota(nota) {
+    const noteList = document.getElementById("note-list");
+    const div = document.createElement("div");
+    div.innerHTML = `
             <div class="card text-center mb-4 pt-3">
                 <div class="card-Body">
                 <p class="border d-inline p-2 mt-3 bg-dark text-light">
@@ -61,66 +69,72 @@ class Intefaz{
                 <p class="mt-3 font-italic fs-2">
                 ${nota.text}
                 </p>
-                    </div>
+                </div>
             </div>
         `;
-        noteList.appendChild(div);
-
-
-    }
-    borrarTextoDeInput(){
-        document.getElementById("annotation-form").reset();
-    }
-    borrarNota(div){
-        if(div.name === "Marcar"){
-            div.parentElement.parentElement.remove();
-            this.mostrarMensaje("Nota marcada satisfactoriamente","success");
-        } 
-    }
-    mostrarMensaje(mensaje, clase){
-        const mensajeContainer = document.createElement("div");
-        mensajeContainer.className = `alert alert-`+clase;
-        mensajeContainer.appendChild(document.createTextNode(mensaje));
-        //Lo que voy a mostrar en el html
-        const container = document.querySelector(".container");
-        const app = document.getElementById("Aplicacion");
-        container.insertBefore(mensajeContainer,app);
-        //evento remover con temporizador
-        setTimeout(removerTimeOut,3000);
-    }
+    noteList.appendChild(div);
+  }
+  borrarTextoDeInput() {
+    document.getElementById("annotation-form").reset();
+  }
+  mostrarMensaje(mensaje, clase) {
+    const mensajeContainer = document.createElement("div");
+    mensajeContainer.className = `alert alert-` + clase;
+    mensajeContainer.appendChild(document.createTextNode(mensaje));
+    //Lo que voy a mostrar en el html
+    const container = document.querySelector(".container");
+    const app = document.getElementById("Aplicacion");
+    container.insertBefore(mensajeContainer, app);
+    //evento remover con temporizador
+    setTimeout(removerTimeOut, 3000);
+  }
 }
 // Eventos
-    document.getElementById("annotation-form").addEventListener("submit", function (e) {
-        const date = document.getElementById("date").value;
-        const text = document.getElementById("text").value;
-        //Agrego en un objeto
-        let myObj = {Date : date, Note : text};
-        let myObjJSON = JSON.stringify(myObj);
-        //Agrego a un Array
-        let myArray = JSON.parse(localStorage.getItem("DataDatos")) || [];
-        myArray.push(myObj);
-            //Convierto el myArray a JSON
-        let myArrayJSON = JSON.stringify(myArray);
-        //LocalStorage
-        localStorage.setItem("DataDatos", myArrayJSON);
+document
+  .getElementById("annotation-form")
+  .addEventListener("submit", function (e) {
+    const date = document.getElementById("date").value;
+    const text = document.getElementById("text").value;
+    //Agrego en un objeto
+    let myObj = { Date: date, Note: text };
+    let myObjJSON = JSON.stringify(myObj);
+    //Agrego a un Array
+    let myArray = JSON.parse(localStorage.getItem("DataDatos")) || [];
+    myArray.push(myObj);
+    //Convierto el myArray a JSON
+    let myArrayJSON = JSON.stringify(myArray);
+    //LocalStorage
+    localStorage.setItem("DataDatos", myArrayJSON);
 
-        const nota = new Nota(date,text);
-        const interfaz = new Intefaz();
-
-            (date === "" || text === "") && interfaz.mostrarMensaje("No se puede agregar un campo de texto vacio, por favor completelo para poder agregar su nota. Gracias","danger");
-
-        interfaz.agregarNota(nota);
-        interfaz.borrarTextoDeInput();
-        interfaz.mostrarMensaje("Nota agregada satisfactoriamente","success");
-        e.preventDefault();
-    });
-
-    document.getElementById("note-list").addEventListener("click", function(e){
-        const interfaz = new Intefaz();
-        interfaz.borrarNota(e.target);
-    })
-
-    //functions
-    function removerTimeOut(){
-        document.querySelector(".alert").remove();
-    }
+    const nota = new Nota(date, text);
+    const interfaz = new Interfaz();
+    interfaz.agregarNota(nota);
+    interfaz.borrarTextoDeInput();
+    interfaz.mostrarMensaje("Nota agregada satisfactoriamente", "success");
+    e.preventDefault();
+  });
+//functions
+function removerTimeOut() {
+  document.querySelector(".alert").remove();
+}
+function insertarImagenFondo() {
+  /*const imageArray = [
+    'url("https://p4.wallpaperbetter.com/wallpaper/362/276/920/nature-4k-pc-full-hd-wallpaper-preview.jpg")',
+    'url("https://p4.wallpaperbetter.com/wallpaper/466/795/642/full-hd-nature-picture-download-1920x1200-wallpaper-preview.jpg")',
+    'url("https://c4.wallpaperflare.com/wallpaper/632/162/458/lago-naturaleza-4k-8k-wallpaper-preview.jpg")',
+    'url("https://i.pinimg.com/originals/7c/e0/bf/7ce0bf1f4c244dd647f7dd7c8d58e69c.jpg")',
+    'url("https://c4.wallpaperflare.com/wallpaper/566/706/505/bosque-naturaleza-paisaje-wallpaper-preview.jpg")',
+  ];*/
+  fetch("../data/data_Json.json")
+    .then((response) => response.json())
+    .then((imagenes) => insertarImagenFondoEnArray(imagenes));
+}
+function insertarImagenFondoEnArray(imagenes) {
+  let imageArray = [];
+  imagenes.forEach((element) => {
+    imageArray.push("url(" + element + ")");
+  });
+  const backgroundContainer = document.getElementById("backgroundContainer");
+  const bg = imageArray[Math.floor(Math.random() * imageArray.length)];
+  backgroundContainer.style.backgroundImage = bg;
+}
--- script.js
+++ script.js
@@ -17,76 +17,112 @@ let formulario__login = document.getElementById("formulario__login");
 let formulario__register = document.getElementById("formulario__register");
 let box__dorso_login = document.getElementById("box__dorso-login");
 let box__dorso_register = document.getElementById("box__dorso-register");
-window.addEventListener("resize",reajustar);
-function reajustar(){
-    if(window.innerWidth > 850){
-        box__dorso_login.style.display = "block";
-        box__dorso_register.style.display = "block";
-    }else{
-        box__dorso_register.style.display = "block";
-        box__dorso_register.style.opacity = "1";
-        box__dorso_login.style.display = "none";
-        formulario__login.style.display = "block";
-        formulario__register.style.display = "none";
-        box__login_register.style.left = "0px";
-
-    }
-}
-
-
-function loguearse(){
-    if(window.innerWidth > 850){
-        formulario__register.style.display = "none";
-        box__login_register.style.left = "10px";
-        formulario__login.style.display = "block";
-        box__dorso_login.style.opacity = "0";
-        box__dorso_register.style.opacity = "1";
-    }else
+window.addEventListener("resize", reajustar);
+function reajustar() {
+  if (window.innerWidth > 850) {
+    box__dorso_login.style.display = "block";
+    box__dorso_register.style.display = "block";
+  } else {
+    box__dorso_register.style.display = "block";
+    box__dorso_register.style.opacity = "1";
+    box__dorso_login.style.display = "none";
+    formulario__login.style.display = "block";
     formulario__register.style.display = "none";
     box__login_register.style.left = "0px";
+  }
+}
+
+function loguearse() {
+  if (window.innerWidth > 850) {
+    formulario__register.style.display = "none";
+    box__login_register.style.left = "10px";
     formulario__login.style.display = "block";
-    box__dorso_login.style.display = "none";
-    box__dorso_register.style.display = "block";
+    box__dorso_login.style.opacity = "0";
+    box__dorso_register.style.opacity = "1";
+    formulario__register.reset();
+  } else formulario__register.style.display = "none";
+  box__login_register.style.left = "0px";
+  formulario__login.style.display = "block";
+  box__dorso_login.style.display = "none";
+  box__dorso_register.style.display = "block";
+  formulario__register.reset();
 }
-function registrarse(){
-    if(window.innerWidth >850){
-        formulario__register.style.display = "block";
-        box__login_register.style.left = "410px";
-        formulario__login.style.display = "none";
-        box__dorso_login.style.opacity = "1";
-        box__dorso_register.style.opacity = "0";
-    }else{
-        formulario__register.style.display = "block";
-        box__login_register.style.left = "0px";
-        formulario__login.style.display = "none";
-        box__dorso_login.style.display = "block";
-        box__dorso_register.style.display = "none";
-        box__dorso_login.style.opacity = "1"
+function registrarse() {
+  if (window.innerWidth > 850) {
+    formulario__register.style.display = "block";
+    box__login_register.style.left = "410px";
+    formulario__login.style.display = "none";
+    box__dorso_login.style.opacity = "1";
+    box__dorso_register.style.opacity = "0";
+    formulario__login.reset();
+  } else {
+    formulario__register.style.display = "block";
+    box__login_register.style.left = "0px";
+    formulario__login.style.display = "none";
+    box__dorso_login.style.display = "block";
+    box__dorso_register.style.display = "none";
+    box__dorso_login.style.opacity = "1";
+    formulario__login.reset();
+  }
 }
-}
-function loguearUsuario(e){
-    //------------Variables de Loguear Usuario
-    let correo__login = document.getElementById("correo__log").value;
-    let contraseña__login = document.getElementById("password__log").value;
-    let correo__local = localStorage.getItem("correo");
-    let contraseña__local = localStorage.getItem("contraseña");
+function loguearUsuario(e) {
+  e.preventDefault();
+  //------------Variables de Loguear Usuario
+  let correo__login = document.getElementById("correo__log").value;
+  let contraseña__login = document.getElementById("password__log").value;
+  let correo__local = localStorage.getItem("correo");
+  let contraseña__local = localStorage.getItem("contraseña");
 
-    (correo__login != correo__local || contraseña__login != contraseña__local) ? alert("Usuario o contraseña inexistente") : window.location = "anotador.html";
-    e.preventDefault();
+  if (
+    correo__login != correo__local ||
+    contraseña__login != contraseña__local ||
+    correo__login === "" ||
+    contraseña__login === ""
+  ) {
+    Swal.fire({
+      text: "Usuario o contraseña inexistente",
+      confirmButtonText: "Cerrar",
+    });
+  } else {
+    window.location = "./anotador.html";
+    formulario__login.reset();
+  }
 }
-function registrarUsuario(e){
-    //------------Variables de Registrar Usuario
-let nombre__completo = document.getElementById("nombre__completo").value;
-let correo = document.getElementById("correo").value;
-let nombre__usuario = document.getElementById("nombre__usuario").value;
-let contraseña__uno = document.getElementById("contraseña__uno").value;
-let contraseña__dos = document.getElementById("contraseña__dos").value;
+//------------Variables de Registrar Usuario
+function registrarUsuario(e) {
+  e.preventDefault();
+  let nombre__completo = document.getElementById("nombre__completo").value;
+  let correo = document.getElementById("correo").value;
+  let nombre__usuario = document.getElementById("nombre__usuario").value;
+  let contraseña__uno = document.getElementById("contraseña__uno").value;
+  let contraseña__dos = document.getElementById("contraseña__dos").value;
+  let textAlert = document.getElementById("Alerta");
 
-(contraseña__uno != contraseña__dos) ? alert("No coincide la contraseña") : localStorage.setItem("nombre__completo", nombre__completo)
-localStorage.setItem("correo", correo)
-localStorage.setItem("nombre__usuario", nombre__usuario)
-localStorage.setItem("contraseña", contraseña__uno)
-formulario__register.reset();
-loguearse();
-e.preventDefault();
+  const characters = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  let entrar = false;
+  textAlert.innerHTML = "";
+  if (nombre__completo.length < 6) {
+    entrar = true;
+  }
+  if (!characters.test(correo)) {
+    entrar = true;
+  }
+  if (
+    contraseña__uno != contraseña__dos ||
+    contraseña__uno === "" ||
+    contraseña__dos === ""
+  ) {
+    Swal.fire({
+      text: "La contraseña invalida",
+      confirmButtonText: "Cerrar",
+    });
+  } else {
+    localStorage.setItem("nombre__completo", nombre__completo);
+    localStorage.setItem("correo", correo);
+    localStorage.setItem("nombre__usuario", nombre__usuario);
+    localStorage.setItem("contraseña", contraseña__uno);
+    formulario__register.reset();
+    //textAlert.innerHTML = "";
+    loguearse();
+  }
 }
